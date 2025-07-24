3be7b1124b1066244103b21787bee541733733ae
Samuel Coco <Samuel.HenriAC@outlook.com>
2025-07-24 17:18:26 +0100

    Project initalised

---
+++ src/test/java/com/samhcoco/graphs/graphs/GraphsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samhcoco.graphs.graphs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
